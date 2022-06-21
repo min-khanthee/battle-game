759bbae974442f8c64780627a16ee3ac6ceb6120
Min Khanthee <pkhanthee@gmail.com>
2022-06-22 08:56:20 +1200

    refactor innerHTML and create functions to render avatar

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-document.getElementById('hero').innerHTML = `
+const elementIdHero = 'hero'
+const nameHero = 'Wizard'
+const avatarHero = 'images/wizard.png'
+const healthHero = '60'
+const diceHero = 6
+
+const elementId = 'monster'
+const name = 'Orc'
+const avatar = 'images/orc.png'
+const health = '10'
+const dice = 4
+
+function renderCharacter(elementId, name, avatar, health, dice) {
+  document.getElementById(elementId).innerHTML = `
         <div class="character-card">
-          <h4 class="name">Wizard</h4>
-          <img class="avatar" src="images/wizard.png" />
-          <p class="health">health: <b> 60 </b></p>
-          <div class="dice-container"><div class="dice">6</div></div>
+          <h4 class="name">${name}</h4>
+          <img class="avatar" src="${avatar}" />
+          <p class="health">health: <b> ${health} </b></p>
+          <div class="dice-container"><div class="dice">${dice}</div></div>
         </div>
 `
-document.getElementById('monster').innerHTML = `
-        <div class="character-card">
-          <h4 class="name">Orc</h4>
-          <img class="avatar" src="images/orc.png" />
-          <p class="health">health: <b> 10 </b></p>
-          <div class="dice-container"><div class="dice">4</div></div>
-        </div>
-`
+}
+
+renderCharacter('hero', 'Wizard', 'images/wizard.png', '60', 6)
+
+renderCharacter('monster', 'Orc', 'images/orc.png', '10', 4)
