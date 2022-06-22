8e4f4891541276f4f9a9ee6e7daf1cf8372952bc
Min Khanthee <pkhanthee@gmail.com>
2022-06-23 07:54:44 +1200

    add diceCount and for loop to update diceHtml🎲

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const hero = {
   avatar: 'images/wizard.png',
   health: 60,
   diceRoll: 6,
+  diceCount: 3,
 }
 
 const monster = {
@@ -12,16 +13,24 @@ const monster = {
   avatar: 'images/orc.png',
   health: 10,
   diceRoll: 4,
+  diceCount: 1,
 }
 
 function renderCharacter(data) {
-  const { elementId, name, avatar, health, diceRoll } = data
+  const { elementId, name, avatar, health, diceRoll, diceCount } = data
+
+  let diceHtml = ''
+
+  for (let i = 0; i < diceCount; i++) {
+    diceHtml += `<div class="dice">6</div>`
+  }
+
   document.getElementById(elementId).innerHTML = `<div class="character-card">
           <h4 class="name"> ${name} </h4>
           <img class="avatar" src="${avatar}" />
           <div class="health">health: <b> ${health} </b></div>
           <div class="dice-container">
-              <div class="dice"> ${diceRoll} </div>
+              ${diceHtml}
           </div>
       </div>`
 }
