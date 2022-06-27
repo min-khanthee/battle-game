77936f224f84a5c784156b6082ed316fd0b1f7c5
Min Khanthee <pkhanthee@gmail.com>
2022-06-27 16:20:22 +1200

    refactor dice roll function

--- index.js
+++ index.js
@@ -15,10 +15,9 @@ const monster = {
 }
 
 function getDiceRollArray(diceCount) {
-  const newDiceRolls = []
-  for (let i = 0; i < diceCount; i++) {
-    newDiceRolls.push(Math.floor(Math.random() * 6) + 1)
-  }
+  return new Array(diceCount).fill(0).map(function () {
+    return Math.floor(Math.random() * 6) + 1
+  })
   return newDiceRolls
 }
 
