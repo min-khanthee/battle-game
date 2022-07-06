f5bcb196f66d9405a82e4fab0e05fe0e7db60879
Min Khanthee <pkhanthee@gmail.com>
2022-07-07 06:32:13 +1200

    render healthbar💚

--- Character.js
+++ Character.js
@@ -6,8 +6,8 @@ const getPercentage = (remainingHealth, maximumHealth) => (100 * remainingHealth
 
 function Character(data) {
   Object.assign(this, data)
-
   this.maxHealth = this.health
+  this.diceArray = getDicePlaceholderHtml(this.diceCount)
 
   this.takeDamage = function (attackScoreArray) {
     const totalAttackScore = attackScoreArray.reduce((total, num) => total + num)
@@ -16,14 +16,18 @@ function Character(data) {
       this.dead = true
       this.health = 0
     }
-    console.log(getPercentage(this.health, this.maxHealth))
   }
-  this.diceArray = getDicePlaceholderHtml(this.diceCount)
+
   //methods on constructor function
 
   this.getHealthBarHtml = function () {
     const percent = getPercentage(this.health, this.maxHealth)
-    console.log(percent)
+    return `
+    <div class="health-bar-outer">
+        <div class="health-bar-inner ${percent < 26 ? 'danger' : ''} " 
+        style="width: ${percent}%;">
+        </div>
+    </div>`
   }
 
   this.getDiceHtml = function () {
@@ -34,12 +38,12 @@ function Character(data) {
   this.getCharacterHtml = function () {
     const { elementId, name, avatar, health, diceCount, diceArray } = this
     const healthBar = this.getHealthBarHtml()
-    const diceHtml = this.getDiceHtml(diceCount)
 
     return `<div class="character-card">
         <h4 class="name"> ${name} </h4>
         <img class="avatar" src="${avatar}" />
         <div class="health">health: <b> ${health} </b></div>
+        ${healthBar}
         <div class="dice-container">    
             ${diceArray}
         </div>
