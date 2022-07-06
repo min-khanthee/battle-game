2439083ef124994febdb76e84ccac71eee3c36e8
Min Khanthee <pkhanthee@gmail.com>
2022-07-07 07:07:39 +1200

    refactor

--- Character.js
+++ Character.js
@@ -1,6 +1,4 @@
-import { getDiceRollArray, getDicePlaceholderHtml } from '/utils.js'
-
-const getPercentage = (remainingHealth, maximumHealth) => (100 * remainingHealth) / maximumHealth
+import { getDiceRollArray, getDicePlaceholderHtml, getPercentage } from '/utils.js'
 
 //Constructor function
 
@@ -36,18 +34,18 @@ function Character(data) {
   }
 
   this.getCharacterHtml = function () {
-    const { elementId, name, avatar, health, diceCount, diceArray } = this
+    const { elementId, name, avatar, health, diceCount } = this
     const healthBar = this.getHealthBarHtml()
-
-    return `<div class="character-card">
-        <h4 class="name"> ${name} </h4>
-        <img class="avatar" src="${avatar}" />
-        <div class="health">health: <b> ${health} </b></div>
-        ${healthBar}
-        <div class="dice-container">    
-            ${diceArray}
-        </div>
-    </div>`
+    return `
+        <div class="character-card">
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}" />
+            <div class="health">health: <b> ${health} </b></div>
+            ${healthBar}
+            <div class="dice-container">
+                ${this.diceArray}
+            </div>
+        </div>`
   }
 }
 
--- data.js
+++ data.js
@@ -4,12 +4,14 @@ const characterData = {
     avatar: 'images/wizard.png',
     health: 60,
     diceCount: 3,
+    currentDiceScore: [],
   },
   monster: {
     name: 'Orc',
     avatar: 'images/orc.png',
     health: 55,
     diceCount: 3,
+    currentDiceScore: [],
   },
 }
 
