658a06e050411ac45014e1f48a7a7d3a99a8d927
Min Khanthee <pkhanthee@gmail.com>
2022-07-08 08:24:36 +1200

    add settimeout ⏲

--- index.js
+++ index.js
@@ -15,8 +15,10 @@ function attack() {
     endGame()
   } else if (monster.dead) {
     if (monstersArray.length > 0) {
-      monster = getNewMonster()
-      render()
+      setTimeout(() => {
+        monster = getNewMonster()
+        render()
+      }, 1500)
     } else {
       endGame()
     }
@@ -38,11 +40,13 @@ function endGame() {
 
   const endEmoji = wizard.health > 0 ? '🔮' : '☠️'
 
-  document.body.innerHTML = `<div class="end-game">
-      <h2>Game Over</h2>
-      <h3>${endMessage}</h3>
-      <p class="end-emoji">${endEmoji}</p>
-  </div>`
+  setTimeout(() => {
+    document.body.innerHTML = `<div class="end-game">
+    <h2>Game Over</h2>
+    <h3>${endMessage}</h3>
+    <p class="end-emoji">${endEmoji}</p>
+</div>`
+  }, 1500)
 }
 
 function render() {
